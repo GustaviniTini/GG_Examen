39b1afd2a08267a300ddcab9468c6f6db7d6ad93
GustaviniTini <gustavoegn2101@gmail.com>
2023-04-24 08:20:38 -0500

    Creacion de los data anotation

--- Models/GGuevara.cs
+++ Models/GGuevara.cs
@@ -4,14 +4,20 @@ namespace GG_Examen.Models
 {
     public class GGuevara
     {
-        public int gg_codigo { get; set; }
+        public int gg_Codigo { get; set; }
         [Required]
-        public string? gg_nombre { get; set; }
+        [StringLength(15)]
+        public string? gg_Nombre { get; set; }
         public bool gg_EnStock { get; set; }
 
         [Range(0.01, 9999.99)]
+        [RegularExpression(@"^\$?\d+(\.(\d{2}))?$")]
         public decimal gg_Precio { get; set; }
 
-    
+        public DateTime gg_FechaCaducidad { get; set; }
+
+        [EmailAddress]
+        public string? gg_CorreoClienteQueAdquirioProducto { get; set; }
+
     }
 }
