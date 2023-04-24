7349fa9d15f462c5c3a92abea7cbc7df873edaeb
GustaviniTini <gustavoegn2101@gmail.com>
2023-04-24 08:27:41 -0500

    Creacion del context

---
+++ Data/GG_ExamenContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GG_Examen.Models;
+
+namespace GG_Examen.Data
+{
+    public class GG_ExamenContext : DbContext
+    {
+        public GG_ExamenContext (DbContextOptions<GG_ExamenContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GG_Examen.Models.GGuevara> GGuevara { get; set; } = default!;
+    }
+}
