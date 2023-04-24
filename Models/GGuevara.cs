9921b0c5f2f812d796e040b28e29fd2da038643f
Laboratorio <laboratorio@udla-ec.int>
2023-04-24 07:54:44 -0500

    Personalizacion de la tabla

---
+++ Models/GGuevara.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GG_Examen.Models
+{
+    public class GGuevara
+    {
+        public int gg_codigo { get; set; }
+        [Required]
+        public string? gg_nombre { get; set; }
+        public bool gg_EnStock { get; set; }
+
+        [Range(0.01, 9999.99)]
+        public decimal gg_Precio { get; set; }
+
+    
+    }
+}
